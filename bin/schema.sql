8e77f4ee04986dda313d180cfe8734865ce7c969
Hrafn Eiríksson <hrafne@gmail.com>
2015-09-25 10:42:37 +0000

    Did things.

--- bin/schema.sql
+++ bin/schema.sql
@@ -5,8 +5,8 @@
 CREATE TABLE IF NOT EXISTS "Accounts" (
     id uuid NOT NULL,
 
-    email character varying(255),
-    password character varying(255),
+    "email" character varying(255),
+    "password" character varying(255),
 
     "name" character varying(255),
     "avatarUrl" character varying(255),
@@ -28,5 +28,11 @@ CREATE TABLE IF NOT EXISTS "Recipes" (
 */
 
 ###
-### TABLE: 
+### TABLE: Products
 ###
+CREATE TABLE IF NOT EXISTS "Products" (
+  id uuid NOT NULL,
+
+  "barcode" character varying(64),
+  "title" character varying(128)
+);
